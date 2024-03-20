27ec9e6b0bb430ff482436955d5edc326743635e
s19124 <s19124@pjwstk.edu.pl>
2024-03-21 00:00:09 +0100

    Zadanie 3

--- Cw1-Git/Cw1-Git/Program.cs
+++ Cw1-Git/Cw1-Git/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World! One. Two. Three.");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class program
+{
+    public void sumAverageElements(int[] arr, int size)
+    {
+ 
+        int sum = 0;
+        int average = 0;
+        for (int i = 0; i < size; i++)
+        {
+            sum += arr[i];
+        }
+        average = sum / size; 
+        Console.WriteLine("Sum Of Array is : " + sum);
+        Console.WriteLine("Average Of Array is : " + average);
+        Console.ReadLine();
+    }
+    public static void Main(string[] args)
+    {
+        int size;
+        Console.WriteLine("Enter the Size :");
+        size = Convert.ToInt32(Console.ReadLine());
+        int[] a = new int[size];
+        Console.WriteLine("Enter the Elements of the Array : ");
+        for (int i = 0; i < size; i++)
+        {
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int len = a.Length;
+        program pg = new program();
+        pg.sumAverageElements(a, len);
+    }
+}
